b0e63a6e4274f414cdc407aacdb49cf77a2fd2a9
kauagabri333 <135608394+kauagabri333@users.noreply.github.com>
2023-10-02 08:04:54 -0300

    Add files via upload

---
+++ aluramid-main/kaua-aluramidi-curso/main.js
@@ -0,0 +1,20 @@
+function tocasompom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocasomclap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+
+function tocasomtim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
