f7d319d223fc919f6787d9466a8896f8d3293082
kauagabri333 <135608394+kauagabri333@users.noreply.github.com>
2023-10-09 08:03:32 -0300

    Add files via upload

--- aluramid-main/kaua-aluramidi-curso/main.js
+++ aluramid-main/kaua-aluramidi-curso/main.js
@@ -3,7 +3,7 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_Pom').play();
 }
 
-document.querySelector('.tecla_Pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 
